fa6ef02798f96f4b4e1b986dfdbd1f5a89eb451c
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2025-09-03 22:32:03 +0700

    Fix urls.py

--- bank/urls.py
+++ bank/urls.py
@@ -1,19 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-<<<<<<< HEAD
+from . import views
+
 from bank import views
 
-urlpatterns = [
+urlpatterns = [ 
     path('', views.menu, name='menu'),  # หน้าแรก
     path('menu/', views.menu, name='menu'),
     path('home/', views.home, name='home'),
     path('login/', views.login_view, name='login'),
-=======
-from . import views
-
-urlpatterns = [
-    path('', views.base, name='base'),
-    path('home/', views.home, name='home'),
-    path('menu/', views.menu, name='menu'),
->>>>>>> feature-menu
 ]
