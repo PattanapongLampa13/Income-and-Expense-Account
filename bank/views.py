6e52935c83640dfc9cb74704a7c727c7e7f9e8eb
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2025-09-03 22:25:06 +0700

    Fix Fail merge 2

--- bank/views.py
+++ bank/views.py
@@ -1,4 +1,37 @@
 
 from django.shortcuts import render
 
-# Create your views here.
+<<<<<<< HEAD
+# views.py
+from django.shortcuts import render, redirect  # render และ redirect
+from django.contrib.auth import authenticate, login  # authenticate, login
+from django.contrib import messages  # messages
+
+
+=======
+>>>>>>> feature-menu
+def base(request):
+	return render(request, 'base.html')
+
+def home(request):
+	return render(request, 'home.html')
+
+def menu(request):
+<<<<<<< HEAD
+
+	return render(request, 'menu.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # เปลี่ยนเป็นชื่อ URL หน้า Home ของคุณ
+        else:
+            messages.error(request, "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง")
+    return render(request, 'account/login.html')
+=======
+	return render(request, 'menu.html')
+>>>>>>> feature-menu
