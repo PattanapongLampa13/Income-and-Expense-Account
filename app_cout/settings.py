fa93f3c10b721108cbdada33f66c32fa22041f29
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-09-04 18:03:07 +0700

    Settings and vercel commit add on settings

--- app_cout/settings.py
+++ app_cout/settings.py
@@ -75,15 +75,16 @@ WSGI_APPLICATION = "app_cout.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.hbnbkwzafxfymwrgxrrs',
+        'PASSWORD': os.environ.get('SUPABASE_DB_PASSWORD', 'Boonnara6712732105'),
+        'HOST': 'aws-1-ap-southeast-1.pooler.supabase.com',
+        'PORT': '5432',
     }
 }
+ 
 
 
 # Password validation
@@ -120,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'statics')]
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Redirect to login page if not authenticated
