89433316f99113977fc574a92001037622de6d90
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-09-04 18:55:36 +0700

    Update static file settings in settings.py for improved structure

--- app_cout/settings.py
+++ app_cout/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [Path.joinpath(BASE_DIR, 'static')]
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles_build', 'static')
 
 # Redirect to login page if not authenticated
 LOGIN_URL = 'login'
