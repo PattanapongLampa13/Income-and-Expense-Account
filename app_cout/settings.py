fe344c3afe62e1367819b920c8f2bd4fdd4c9cf6
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-09-04 17:52:58 +0700

    settings fix

--- app_cout/settings.py
+++ app_cout/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Redirect to login page if not authenticated
 LOGIN_URL = 'login'
