8b9f46e184d44503fee28c72040bfd93f8988ffe
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-09-04 18:26:39 +0700

    Settings and vercel

--- app_cout/settings.py
+++ app_cout/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-^f1j)28)dprg7+xfd+a7tl@gbhh@rutr-4yg^=x#kx6&@0a)&+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # โฟลเดอร์สำหรับไฟล์ static ในโปรเจค
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Redirect to login page if not authenticated
